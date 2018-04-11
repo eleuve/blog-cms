1d9665f3a728c086f734fdf39e107ef798d82edc
DESKTOP-PGRQR98\iconestudio <lauravicmartin@gmail.com>
2018-04-11 17:59:51 +0200

    cambios a medias de borrar-post

--- public/admin/borrar-post.php
+++ public/admin/borrar-post.php
@@ -4,7 +4,10 @@ include_once '../../DB/conexion.php';
 
 
 if (isset($_GET["id"])) {
-    $sql = "DELETE * FROM post WHERE id = " . $_GET["id"];
+    $sql = "SELECT imagen from post WHERE idpost = " . $_GET["id"];
+
+
+    $sql = "DELETE FROM post WHERE idpost = " . $_GET["id"];
     
     // ejecuta la sql com oya sabes
     
@@ -15,6 +18,7 @@ if (isset($_GET["id"])) {
     $mensaje = "Post borrado correctamente.";
 
     if (mysqli_errno($con)) {
+        print_r(mysqli_error($con));
         $mensaje = "Intentelo de nuevo mas tarde o contacte con el administrador";
     }
 
