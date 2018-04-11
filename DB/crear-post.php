8d966909761be42e355d082636da48f3a581e2e8
DESKTOP-PGRQR98\iconestudio <lauravicmartin@gmail.com>
2018-04-11 15:39:48 +0200

    formulario básico

---
+++ DB/crear-post.php
@@ -0,0 +1,15 @@
+<?php
+?>
+
+<form action="">
+	<label for="">Título:</label>
+	<input type="text">
+	<label for="">Entradilla:</label>
+	<input type="text">
+	<label for="">Imagen:</label>
+	<input type="file" />
+	<label for="">Entrada:</label>
+	<textarea name="" id="" cols="30" rows="40"></textarea>
+	<input type="checkbox" name="publico" value="público">Público<br>
+	<input type="submit" value="Enviar">
+</form>
