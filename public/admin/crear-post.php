e134f273d6974c455293decd8b6f62fbe4524ff5
Carlos Vicente Martin <carlos.vicente@clifton-asset.co.uk>
2018-04-11 15:37:12 +0100

    Add post guardado en la base de datos y funcionando

--- public/admin/crear-post.php
+++ public/admin/crear-post.php
@@ -1,16 +1,65 @@
 <?php
 // IF que solo se ejecuta si hay POST enviar (es el submit input)
 if (isset($_POST["enviar"])) {
-    // guardamos el archivo que sube en la carpeta /uploads
+    /**
+     * @todo Asegurarnos que las 5 variables siguientes son aptas, es decir, not-empty o... con valores raros o SQL code (injection...)
+     */
+    $titulo = $_POST["titulo"];
+    $entradilla = $_POST["entradilla"];
     $file = $_FILES["imagen"];
+    $html = $_POST["entrada"];
+    $esPublico = "false";
 
+    if (isset($_POST["publico"])) {
+        if ($_POST["publico"] == "público") {
+            $esPublico = "true";
+        }
+    }
+
+    /** Guardado del archivo en la carpeta /uploads */
     // El archivo se sube a una ruta temporal de PHP (a saber cual), no hay funcion MOVE en php, asi que leemos el contenido del archivo...
     $imageBinaryData = file_get_contents($file["tmp_name"]);
-
     // Crea el archivo en uploads, require 2 cosas: nombre de archivo, contenido
     file_put_contents('../uploads/' . $file["name"], $imageBinaryData);
 
-    echo "<h1>Ok, echa un vistazo en la carpeta /uploads/ a ver si esta el archivo:" . $file["name"] . "</h1>";
+    /** Como todo es correcto (validado empty & injections), ejecutamos un INSERT... */
+
+    // Necesitamos la variable que nos facilita conexion de la base de datos: $con
+    require_once "../../DB/conexion.php";
+
+    // Preparamos la fecha para el SQL
+    $datetimeHoy = new \Datetime("now");
+    $fechaHoy = date("Y-m-d H:i:s", $datetimeHoy->getTimestamp());
+
+    // Preparamos el SQL
+    $sql = sprintf(
+        "INSERT INTO `post` (`idpost`, `titulo`, `entradilla`, `contenido`, `fecha`, `categoria`, `imagen`, `activo`) VALUES (%s, '%s', '%s', '%s', '%s', '%s', '%s', %s)",
+        "NULL",
+        $titulo,
+        $entradilla,
+        $html,
+        $fechaHoy,
+        "Categoria..",
+        $file["name"],
+        $esPublico
+    );
+
+    // Ejecutamos el SQL con la respectiva conexion ($con)
+    $resultadoDelQuery = mysqli_query($con, $sql);
+
+    // la variable que esto devuelve no la necesitamos para nada  de momento pero ahi queda... servira para ver si no ha guardado el INSERT,
+    // mostrar un mensaje rollo 'Intentelo de nuevo mas tarde o contacte con el administrador'
+
+    // si hay error de cualquier tipo, mostramos un mensaje, en caso contrario mostramos otro
+    $mensaje = "Post creado correctamente: " . $_POST["titulo"];
+    if (mysqli_errno($con)) {
+        $mensaje = "Intentelo de nuevo mas tarde o contacte con el administrador";
+    }
+
+    echo "<h1>" . $mensaje . "</h1>";
+
+    // Cerramos la conexion porque hemos acabado
+    mysqli_close($con);
 }
 
 ?>
