be8df881e499befb4972919b5b5a644ed52a0d6b
Carlos Vicente Martin <carlos.vicente@clifton-asset.co.uk>
2018-04-11 16:08:31 +0100

    Add listado de post en pagina principal, con imagenes...

--- public/index.php
+++ public/index.php
@@ -2,15 +2,27 @@
 
 include_once '../DB/conexion.php';
 
-// Check connection
-if (mysqli_connect_errno())
-{
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+
+// vamos a listar los posts que hay en la base de datos...
+
+$sql = "SELECT * FROM post WHERE activo = 1";
+
+$result = mysqli_query($con, $sql);
+
+while ($post = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+    ?>
+    <div>
+        <h2><?php echo $post["titulo"]; ?></h2>
+        <h3><?php echo $post["entradilla"]; ?></h3>
+        <?php echo $post["fecha"]; ?>
+        
+        <img src="uploads/<?php echo $post["imagen"] ?>" width="200">
+
+        <p><?php echo $post["contenido"]; ?></p>
+        
+    </div>
+    <?php
 }
-
-
-print_r($con);
-
 
 
 ?>
