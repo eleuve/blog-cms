a82232ddd50c73d4b3c111b0fcb2d755928f172f
DESKTOP-PGRQR98\iconestudio <lauravicmartin@gmail.com>
2018-04-12 10:18:35 +0200

    borrar imagen cuando se borra un post

--- public/admin/borrar-post.php
+++ public/admin/borrar-post.php
@@ -6,6 +6,20 @@ include_once '../../DB/conexion.php';
 if (isset($_GET["id"])) {
     $sql = "SELECT imagen from post WHERE idpost = " . $_GET["id"];
 
+    //ejecutamos la query
+    $resultadoDelQuery = mysqli_query($con, $sql);
+
+    while($row = $resultadoDelQuery->fetch_assoc()) {
+      echo $row["imagen"];
+    }
+
+    unlink('../uploads/' . $row["imagen"]);
+
+    /*if(file_exists($file)) {
+    } else {
+        echo "archivo no encontrado";
+    }
+    */
 
     $sql = "DELETE FROM post WHERE idpost = " . $_GET["id"];
     
@@ -19,7 +33,7 @@ if (isset($_GET["id"])) {
 
     if (mysqli_errno($con)) {
         print_r(mysqli_error($con));
-        $mensaje = "Intentelo de nuevo mas tarde o contacte con el administrador";
+        $mensaje = "Inténtelo de nuevo más tarde o contacte con el administrador";
     }
 
     echo "<h1>" . $mensaje . "</h1>";
--- public/admin/modificar-post.php
+++ public/admin/modificar-post.php
@@ -4,31 +4,105 @@ include_once '../../DB/conexion.php';
 
 
 if (isset($_GET["id"])) {
-    $sql = "SELECT imagen from post WHERE idpost = " . $_GET["id"];
 
+    $sql = "SELECT * from post WHERE idpost = " . $_GET["id"];
 
-    $sql = "DELETE FROM post WHERE idpost = " . $_GET["id"];
-    
-    // ejecuta la sql com oya sabes
-    
-    // muestra mensaje: borrado
+    $titulo = $_POST["titulo"];
+    $entradilla = $_POST["entradilla"];
+    $file = $_FILES["imagen"];
+    $html = $_POST["entrada"];
+    $esPublico = "false";
+    $altimagen = $_POST["altimagen"];
 
-    $resultadoDelQuery = mysqli_query($con, $sql);
+// IF que solo se ejecuta si hay POST enviar (es el submit input)
+    if (isset($_POST["enviar"])) {
+        /**
+         * @todo Asegurarnos que las 6 variables siguientes son aptas, es decir, not-empty o... con valores raros o SQL code (injection...)
+         */
+        $titulo = $_POST["titulo"];
+        $entradilla = $_POST["entradilla"];
+        $file = $_FILES["imagen"];
+        $html = $_POST["entrada"];
+        $esPublico = "false";
+        $altimagen = $_POST["altimagen"];
 
-    $mensaje = "Post borrado correctamente.";
+        if (isset($_POST["publico"])) {
+            if ($_POST["publico"] == "público") {
+                $esPublico = "true";
+            }
+        }
 
-    if (mysqli_errno($con)) {
-        print_r(mysqli_error($con));
-        $mensaje = "Intentelo de nuevo mas tarde o contacte con el administrador";
+        /** Guardado del archivo en la carpeta /uploads */
+        // El archivo se sube a una ruta temporal de PHP (a saber cual), no hay funcion MOVE en php, asi que leemos el contenido del archivo...
+        $imageBinaryData = file_get_contents($file["tmp_name"]);
+        // Crea el archivo en uploads, require 2 cosas: nombre de archivo, contenido
+        file_put_contents('../uploads/' . $file["name"], $imageBinaryData);
+
+        /** Como todo es correcto (validado empty & injections), ejecutamos un INSERT... */
+
+        // Necesitamos la variable que nos facilita conexion de la base de datos: $con
+        require_once "../../DB/conexion.php";
+
+        // Preparamos la fecha para el SQL
+        $datetimeHoy = new \Datetime("now");
+        $fechaHoy = date("Y-m-d H:i:s", $datetimeHoy->getTimestamp());
+
+        // Preparamos el SQL
+        $sql = sprintf(
+            "INSERT INTO `post` (`idpost`, `titulo`, `entradilla`, `contenido`, `fecha`, `categoria`, `imagen`, `activo`, `altimagen`) VALUES (%s, '%s', '%s', '%s', '%s', '%s', '%s', %s, '%s')",
+            "NULL",
+            $titulo,
+            $entradilla,
+            $html,
+            $fechaHoy,
+            "Categoria..",
+            $file["name"],
+            $esPublico,
+            $altimagen
+        );
+
+        // Ejecutamos el SQL con la respectiva conexion ($con)
+        $resultadoDelQuery = mysqli_query($con, $sql);
+
+        // la variable que esto devuelve no la necesitamos para nada  de momento pero ahi queda... servira para ver si no ha guardado el INSERT,
+        // mostrar un mensaje rollo 'Intentelo de nuevo mas tarde o contacte con el administrador'
+
+        // si hay error de cualquier tipo, mostramos un mensaje, en caso contrario mostramos otro
+        $mensaje = "Post creado correctamente: " . $_POST["titulo"];
+        if (mysqli_errno($con)) {
+            print_r(mysqli_error($con));
+            $mensaje = "Inténtelo de nuevo más tarde o contacte con el administrador.";
+        }
+
+        echo "<h1>" . $mensaje . "</h1>";
+
+        // Cerramos la conexion porque hemos acabado
+        mysqli_close($con);
     }
-
-    echo "<h1>" . $mensaje . "</h1>";
-
-    // Cerramos la conexion porque hemos acabado
-    mysqli_close($con);
-
-    }
+}
 
 ?>
+
+
+<form action="modificar-post.php" method="post" enctype="multipart/form-data">
+    <label for="">Título:</label>
+    <input type="text" name="titulo"><br />
+
+    <label for="">Entradilla:</label>
+    <input type="text" name="entradilla"><br />
+
+    <label for="">Imagen:</label>
+    <input type="file" name="imagen" /><br />
+
+    <label for="">Alt de la imagen:</label>
+    <input type="text" name="altimagen" /><br />
+
+    <label for="">Entrada:</label>
+    <textarea name="entrada" id="" cols="30" rows="40"></textarea><br />
+
+    <input type="checkbox" name="publico" value="público" name="publico">Público<br><br />
+
+    <input type="submit" value="Enviar" name="enviar">
+</form>
 
 
