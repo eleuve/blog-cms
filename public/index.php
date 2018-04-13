6a4d2df4f62ece83063e139386746359105d0e89
DESKTOP-PGRQR98\iconestudio <lauravicmartin@gmail.com>
2018-04-13 17:30:11 +0200

    página de blog con bootstrap última entrada

--- public/index.php
+++ public/index.php
@@ -1,28 +1,171 @@
-<?php
 
-include_once '../DB/conexion.php';
+<!DOCTYPE html>
+<html lang="es">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Blog - Francisco Vidal</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/blog-post.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Francisco Vidal</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container pt-5">
+
+      <div class="row">
+
+        <!-- Post Content Column -->
+        <div class="col-lg-8">
+
+        	<?php
+
+        	include_once '../DB/conexion.php';
 
 
-// vamos a listar los posts que hay en la base de datos...
+			// vamos a listar los posts que hay en la base de datos...
 
-$sql = "SELECT * FROM post WHERE activo = 1";
+        	$sql = "SELECT * FROM post WHERE activo = 1 ORDER BY fecha DESC LIMIT 1";
 
-$result = mysqli_query($con, $sql);
+        	$result = mysqli_query($con, $sql);
 
-while ($post = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-    ?>
-    <div>
-        <h2><?php echo $post["titulo"]; ?></h2>
-        <h3><?php echo $post["entradilla"]; ?></h3>
-        <?php echo $post["fecha"]; ?>
-        
-        <img src="uploads/<?php echo $post["imagen"] ?>" alt="<?php echo $post["altimagen"]?>" width="200">
+        	while ($post = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+        		?>
+        		
 
-        <p><?php echo $post["contenido"]; ?></p>
-        
+          <!-- Title -->
+          <h1 class="mt-4"><?php echo $post["titulo"]; ?></h1>
+
+          <hr>
+
+          <!-- Date/Time -->
+          <p>Publicado <?php echo $post["fecha"]; ?></p>
+
+          <hr>
+
+          <!-- Preview Image -->
+          <img class="img-fluid rounded" src="uploads/<?php echo $post["imagen"] ?>" alt="<?php echo $post["altimagen"]?>" width="900">
+
+          <hr>
+
+          <!-- Post Content -->
+          <?php echo $post["contenido"]; ?>
+
+          <hr>
+
+		<?php
+        	}
+
+
+        	?>
+        </div>
+
+        <!-- Sidebar Widgets Column -->
+        <div class="col-md-4">
+
+          <!-- Categories Widget -->
+          <div class="card my-4">
+            <h5 class="card-header">Categories</h5>
+            <div class="card-body">
+              <div class="row">
+                <div class="col-lg-6">
+                  <ul class="list-unstyled mb-0">
+                    <li>
+                      <a href="#">Web Design</a>
+                    </li>
+                    <li>
+                      <a href="#">HTML</a>
+                    </li>
+                    <li>
+                      <a href="#">Freebies</a>
+                    </li>
+                  </ul>
+                </div>
+                <div class="col-lg-6">
+                  <ul class="list-unstyled mb-0">
+                    <li>
+                      <a href="#">JavaScript</a>
+                    </li>
+                    <li>
+                      <a href="#">CSS</a>
+                    </li>
+                    <li>
+                      <a href="#">Tutorials</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- Side Widget -->
+          <div class="card my-4">
+            <h5 class="card-header">Side Widget</h5>
+            <div class="card-body">
+              You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+      <!-- /.row -->
+
     </div>
-    <?php
-}
+    <!-- /.container -->
 
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Francisco Vidal 2018</p>
+      </div>
+      <!-- /.container -->
+    </footer>
 
-?>
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
+
