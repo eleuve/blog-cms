1feffcc9cfb649e35ca85e565fa9ea0e89a71d14
Carlos Vicente Martin <carlos.vicente@clifton-asset.co.uk>
2018-04-13 13:42:10 +0100

    Add CKeditor in crear-post.php

--- public/admin/crear-post.php
+++ public/admin/crear-post.php
@@ -9,7 +9,7 @@ if (!isset($_SESSION["login"]) || $_SESSION["login"] != true) {
 require_once "../../DB/conexion.php";
 
 // IF que solo se ejecuta si hay POST enviar (es el submit input)
-if (isset($_POST["enviar"])) {
+if (isset($_POST["submit"])) {
     /**
      * @todo Asegurarnos que las 6 variables siguientes son aptas, es decir, not-empty o... con valores raros o SQL code (injection...)
      */
@@ -76,51 +76,116 @@ if (isset($_POST["enviar"])) {
 }
 
 ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>Modificar categoría - Gestor</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <div class="container-fluid">
+        <header class="row">
+            <div class="col-6">
+                <h1>Bienvenido, <?= $_SESSION["username"] ?></h1>
+            </div>
+            <div class="col-6 text-right">
+                <a href="logout.php">Cerrar sesion</a>
+            </div>
+        </header>
+        <div>
+            <i class="fas fa-arrow-circle-left"></i><br />
+            <a href="gestion.php">VOLVER AL MENÚ PRINCIPAL</a>
+        </div>
+    </div>
 
-<a href="gestionPosts.php" class="confirmacion">VOLVER AL MENÚ DE GESTIÓN</a>
 
-<form action="crear-post.php" method="post" enctype="multipart/form-data">
-    <label for="">Categoría</label>
-    <select name="categoria">
-        <?php
-        // Consultamos las categorias e iteramos sobre ellas para imprimir los <options> pertinentes.
-        $resultado = mysqli_query($con, "SELECT * FROM categoria");
-        while ($categoria = mysqli_fetch_array(
-            $resultado,
-            MYSQLI_ASSOC
-        )) {
-            ?><option value="<?= $categoria["idcategoria"] ?>"><?= $categoria["nombre"] ?></option><?php
+
+
+
+    <div class="container-fluid">
+
+        <div class="row">
+            <div class="col-s-12 col-md-9 mx-auto mt-4">
+                <a href="gestionPosts.php" class="confirmacion">VOLVER AL MENÚ DE GESTIÓN</a>
+
+                <div id="form-container" class="container">
+                    <form action="crear-post.php" method="post" enctype="multipart/form-data">
+
+                        <div class="row">
+                            <div class="col-xsl-12">
+                                <div class="form-group">
+                                    <label for="categoria">Categoría</label>
+                                    <select class="form-control" name="categoria">
+                                        <?php
+                                        // Consultamos las categorias e iteramos sobre ellas para imprimir los <options> pertinentes.
+                                        $resultado = mysqli_query($con, "SELECT * FROM categoria");
+                                        while ($categoria = mysqli_fetch_array(
+                                            $resultado,
+                                            MYSQLI_ASSOC
+                                        )) {
+                                            ?><option value="<?= $categoria["idcategoria"] ?>"><?= $categoria["nombre"] ?></option><?php
+                                        }
+                                        ?>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label for="titulo">Título</label>
+                                    <input class="form-control" type="text" name="titulo">
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Entradilla</label>
+                                    <input class="form-control" type="text" name="entradilla"><br/>
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Imagen</label>
+                                    <input class="form-control" type="file" name="imagen"/><br/>
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Alt de la imagen</label>
+                                    <input class="form-control" type="text" name="altimagen"/><br/>
+                                </div>
+                                <div class="form-group">
+                                    <label for="entrada">Entrada</label>
+                                    <textarea class="form-control" name="entrada">
+                                        Whatever
+                                    </textarea>
+                                </div>
+                                <div class="form-group">
+                                    <input type="checkbox" name="publico" value="público" name="publico">Público<br><br/>
+                                </div>
+                                <div class="form-group">
+                                    <button class="btn btn-primary" type="submit" name="submit">Crear</button>
+                                </div>
+                            </div>
+                        </div>
+
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <script src="../js/jquery-3.3.1.min.js"></script>
+    <script src="https://cdn.ckeditor.com/4.9.1/standard/ckeditor.js"></script>
+
+    <script type="text/javascript">
+        var elemento = document.getElementsByClassName('confirmacion');
+        var confirmar = function (e) {
+            if (!confirm('¿Seguro que quieres volver atrás? \n Los cambios que hayas hecho no se guardarán.')) e.preventDefault();
+        };
+        for (var i = 0, l = elemento.length; i < l; i++) {
+            elemento[i].addEventListener('click', confirmar, false);
         }
-        ?>
-    </select><br/>
-
-    <label for="">Título:</label>
-    <input type="text" name="titulo" required><br/>
-
-    <label for="">Entradilla:</label>
-    <input type="text" name="entradilla" required><br/>
-
-    <label for="">Imagen:</label>
-    <input type="file" name="imagen" required/><br/>
-
-    <label for="">Alt de la imagen:</label>
-    <input type="text" name="altimagen"/><br/>
-
-    <label for="">Entrada:</label>
-    <textarea name="entrada" id="" cols="30" rows="40" required></textarea><br/>
-
-    <input type="checkbox" name="publico" value="público" name="publico">Público<br><br/>
-
-    <input type="submit" value="Enviar" name="enviar">
-</form>
 
 
-<script type="text/javascript">
-    var elemento = document.getElementsByClassName('confirmacion');
-    var confirmar = function (e) {
-        if (!confirm('¿Seguro que quieres volver atrás? \n Los cambios que hayas hecho no se guardarán.')) e.preventDefault();
-    };
-    for (var i = 0, l = elemento.length; i < l; i++) {
-        elemento[i].addEventListener('click', confirmar, false);
-    }
-</script>
+    </script>
+    <script type="text/javascript">
+        $('.flash').delay(5000).fadeOut( "slow" );
+
+        CKEDITOR.replace( 'entrada' );
+    </script>
+</body>
+</html>
