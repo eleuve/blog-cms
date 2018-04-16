f2b1a5eb89742d0b830e393fa48eaafc92a7ae06
DESKTOP-PGRQR98\iconestudio <lauravicmartin@gmail.com>
2018-04-16 12:25:26 +0200

    Pequeños cambios estructurales en Entrada

--- public/entrada.php
+++ public/entrada.php
@@ -2,92 +2,92 @@
 <!DOCTYPE html>
 <html lang="es">
 
-  <head>
+<head>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-    <title>Blog - Francisco Vidal</title>
+  <title>Blog - Francisco Vidal</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
-    <link href="css/blog-post.css" rel="stylesheet">
+  <!-- Custom styles for this template -->
+  <link href="css/blog-post.css" rel="stylesheet">
 
-  </head>
+</head>
 
-  <body>
+<body>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="#">Francisco Vidal</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home
-                <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Services</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contact</a>
-            </li>
-          </ul>
-        </div>
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">Francisco Vidal</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
       </div>
-    </nav>
+    </div>
+  </nav>
 
-    <!-- Page Content -->
-    <div class="container pt-5">
+  <!-- Page Content -->
+  <div class="container pt-5">
 
-      <div class="row">
+    <div class="row">
 
-        <!-- Post Content Column -->
-        <div class="col-lg-8">
+      <!-- Post Content Column -->
+      <div class="col-lg-8">
 
-        	<?php
+       <?php
 
-        	include_once '../DB/conexion.php';
+       include_once '../DB/conexion.php';
 
 
 			// vamos a listar los posts que hay en la base de datos...
 
-        	$sql = "SELECT * FROM post WHERE activo = 1 ORDER BY fecha DESC LIMIT 1";
+       $sql = "SELECT * FROM post WHERE activo = 1 ORDER BY fecha DESC LIMIT 1";
 
-        	$result = mysqli_query($con, $sql);
+       $result = mysqli_query($con, $sql);
 
-        	while ($post = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-        		?>
-        		
+       while ($post = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+        ?>
 
-          <!-- Title -->
-          <h1 class="mt-4"><?php echo $post["titulo"]; ?></h1>
-          <h2 class="mt-4"><?php echo $post["entradilla"]; ?></h1>
+
+        <!-- Title -->
+        <h1 class="mt-4"><?php echo $post["titulo"]; ?></h1>
+        <h2 class="mt-4"><?php echo $post["entradilla"]; ?></h1>
 
           <hr>
 
           <?php 
 
-            $idcategoria = $post["idcategoria"];
+          $idcategoria = $post["idcategoria"];
 
-            $consulta = mysqli_query($con, "SELECT nombre FROM categoria WHERE idcategoria=" . $idcategoria);
+          $consulta = mysqli_query($con, "SELECT nombre FROM categoria WHERE idcategoria=" . $idcategoria);
 
-            $row = $consulta->fetch_assoc();
+          $row = $consulta->fetch_assoc();
 
-            $fecha = strtotime($post["fecha"]);
-            $formatoFecha = date("d/m/y H:i", $fecha)
+          $fecha = strtotime($post["fecha"]);
+          $formatoFecha = date("d/m/y H:i", $fecha)
 
           ?>
 
@@ -106,35 +106,33 @@
 
           <hr>
 
-		<?php
-        	}
+          <?php
+        }
 
 
-        	?>
-        </div>
+        ?>
+      </div>
 
-        <!-- Sidebar Widgets Column -->
-        <div class="col-md-4">
+      <!-- Sidebar Widgets Column -->
+      <div class="col-md-4">
 
-          <!-- Categories Widget -->
-          <div class="card my-4">
-            <h5 class="card-header">Categorías</h5>
-            <div class="card-body">
-              <div class="row">
-                <div class="col-lg-6">
-                	<ul class="list-unstyled mb-0">
-                	 <?php
+        <!-- Categories Widget -->
+        <div class="card my-4">
+          <h5 class="card-header">Categorías</h5>
+          <div class="card-body">
+            <div class="row">
+              <ul class="list-unstyled mb-0">
+              <?php
                       // Consultamos las categorias e iteramos sobre ellas para imprimir los <options> pertinentes.
-                        $resultado = mysqli_query($con, "SELECT * FROM categoria");
-                        while ($categoria = mysqli_fetch_array(
-                            $resultado,MYSQLI_ASSOC)) {
-                        ?>
-                        <li><a href="<?= $categoria["slug"] ?>"><?= $categoria["nombre"] ?></a></li>
-                        <?php
-                        }
-                        ?>
-                  	</ul>
-                </div>
+              $resultado = mysqli_query($con, "SELECT * FROM categoria");
+              while ($categoria = mysqli_fetch_array(
+                $resultado,MYSQLI_ASSOC)) {
+                  ?>
+                  <li><a href="<?= $categoria["slug"] ?>"><?= $categoria["nombre"] ?></a></li>
+                  <?php
+                }
+                ?>
+                </ul>
               </div>
             </div>
           </div>
@@ -180,5 +178,5 @@
 
   </body>
 
-</html>
+  </html>
 
